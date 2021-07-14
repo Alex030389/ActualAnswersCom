a790c3324322874e0597794d88532b81e91bef37
AlexKa <x182838@gmail.com>
2021-07-14 12:17:44 +0300

    page paynow

--- _src/design/script/block/__search-input.js
+++ _src/design/script/block/__search-input.js
@@ -1,15 +1,15 @@
-const searchInput = document.querySelector('[data-js="search-input"]');
-const searchFormBtn = document.querySelector('[data-js="search-form-btn"]');
-const searchFormIcon = document.querySelector('[data-js="search-form-icon"]');
+// const searchInput = document.querySelector('[data-js="search-input"]');
+// const searchFormBtn = document.querySelector('[data-js="search-form-btn"]');
+// const searchFormIcon = document.querySelector('[data-js="search-form-icon"]');
 
-if (searchInput) {
-  searchInput.addEventListener('input', (evt) => {
-    if(evt.target.value.length > 0) {
-      searchFormBtn.removeAttribute('disabled');
-      searchFormIcon.classList.add('search-form__icon--active');
-    } else {
-      searchFormBtn.setAttribute('disabled', 'true');
-      searchFormIcon.classList.remove('search-form__icon--active');
-    }
-  });
-}
+// if (searchInput) {
+//   searchInput.addEventListener('input', (evt) => {
+//     if(evt.target.value.length > 0) {
+//       searchFormBtn.removeAttribute('disabled');
+//       searchFormIcon.classList.add('search-form__icon--active');
+//     } else {
+//       searchFormBtn.setAttribute('disabled', 'true');
+//       searchFormIcon.classList.remove('search-form__icon--active');
+//     }
+//   });
+// }
